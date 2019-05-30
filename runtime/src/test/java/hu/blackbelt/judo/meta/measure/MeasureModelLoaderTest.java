1caef22a3c36c0b9739f9e8ebc52161904b8df18
Jozsef Borcsok <jozsef.borcsok@blackbelt.hu>
2019-05-30 17:09:04 +0200

    update logging and tests

--- runtime/src/test/java/hu/blackbelt/judo/meta/measure/MeasureModelLoaderTest.java
+++ runtime/src/test/java/hu/blackbelt/judo/meta/measure/MeasureModelLoaderTest.java
@@ -5,6 +5,7 @@ import hu.blackbelt.judo.meta.measure.runtime.MeasureModelLoader;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -22,14 +23,24 @@ class MeasureModelLoaderTest {
                 "1.0.0");
 
         for (Iterator<EObject> i = measureModel.getResourceSet().getResource(measureModel.getUri(), false).getAllContents(); i.hasNext(); ) {
-            log.info(i.next().toString());
+            log.debug(i.next().toString());
         }
+
+        final long numberOfBaseMeasures = measureModel.getResourceSet().getResource(measureModel.getUri(), false).getContents().stream()
+                .filter(c -> c instanceof BaseMeasure)
+                .count();
+        Assertions.assertEquals(9L, numberOfBaseMeasures);
+
+        final long numberOfDerivedMeasures = measureModel.getResourceSet().getResource(measureModel.getUri(), false).getContents().stream()
+                .filter(c -> c instanceof DerivedMeasure)
+                .count();
+        Assertions.assertEquals(20L, numberOfDerivedMeasures);
     }
 
-    public File srcDir(){
+    public File srcDir() {
         String relPath = getClass().getProtectionDomain().getCodeSource().getLocation().getFile();
-        File targetDir = new File(relPath+"../../src");
-        if(!targetDir.exists()) {
+        File targetDir = new File(relPath + "../../src");
+        if (!targetDir.exists()) {
             targetDir.mkdir();
         }
         return targetDir;
