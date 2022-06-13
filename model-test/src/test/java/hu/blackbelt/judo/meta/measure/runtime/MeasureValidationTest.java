d62c14b840edb44c10e2e3dd27c0aeb2838120f1
Bence Gelei <bence.gelei@blackbelt.hu>
2022-06-13 10:12:56 +0200

    JNG-3769 Logging improved

--- model-test/src/test/java/hu/blackbelt/judo/meta/measure/runtime/MeasureValidationTest.java
+++ model-test/src/test/java/hu/blackbelt/judo/meta/measure/runtime/MeasureValidationTest.java
@@ -2,27 +2,23 @@ package hu.blackbelt.judo.meta.measure.runtime;
 
 import hu.blackbelt.epsilon.runtime.execution.api.Log;
 import hu.blackbelt.epsilon.runtime.execution.exceptions.EvlScriptExecutionException;
-import hu.blackbelt.epsilon.runtime.execution.impl.Slf4jLog;
+import hu.blackbelt.epsilon.runtime.execution.impl.BufferedSlf4jLogger;
 import hu.blackbelt.judo.meta.measure.support.MeasureModelResourceSupport;
-
+import lombok.extern.slf4j.Slf4j;
 import org.eclipse.emf.common.util.URI;
 import org.junit.jupiter.api.BeforeEach;
-import org.slf4j.LoggerFactory;
-import org.slf4j.Logger;
 
 import java.util.Collection;
 
 import static hu.blackbelt.judo.meta.measure.support.MeasureModelResourceSupport.measureModelResourceSupportBuilder;
 
-
+@Slf4j
 public class MeasureValidationTest {
 
     MeasureModelResourceSupport measureModelSupport;
-    
-    private static final Logger logger = LoggerFactory.getLogger(MeasureValidationTest.class);
+
     private final String createdSourceModelName = "urn:Measure.model";
 
-    private Log log = new Slf4jLog();
     private MeasureModel measureModel;
     @BeforeEach
     void setUp() {
@@ -30,7 +26,7 @@ public class MeasureValidationTest {
         measureModelSupport = measureModelResourceSupportBuilder()
                 .uri(URI.createFileURI(createdSourceModelName))
                 .build();
-        
+
         measureModel = MeasureModel.buildMeasureModel()
         		.measureModelResourceSupport(measureModelSupport)
                 .name("test")
@@ -39,20 +35,20 @@ public class MeasureValidationTest {
 
 
     private void runEpsilon (Collection<String> expectedErrors, Collection<String> expectedWarnings) throws Exception {
-        try {
-            MeasureEpsilonValidator.validateMeasure(log,
+        try (Log bufferedLog = new BufferedSlf4jLogger(log)) {
+            MeasureEpsilonValidator.validateMeasure(bufferedLog,
             		measureModel,
                     MeasureEpsilonValidator.calculateMeasureValidationScriptURI(),
                     expectedErrors,
                     expectedWarnings);
         } catch (EvlScriptExecutionException ex) {
-            logger.error("EVL failed", ex);
-            logger.error("\u001B[31m - expected errors: {}\u001B[0m", expectedErrors);
-            logger.error("\u001B[31m - unexpected errors: {}\u001B[0m", ex.getUnexpectedErrors());
-            logger.error("\u001B[31m - errors not found: {}\u001B[0m", ex.getErrorsNotFound());
-            logger.error("\u001B[33m - expected warnings: {}\u001B[0m", expectedWarnings);
-            logger.error("\u001B[33m - unexpected warnings: {}\u001B[0m", ex.getUnexpectedWarnings());
-            logger.error("\u001B[33m - warnings not found: {}\u001B[0m", ex.getWarningsNotFound());
+            log.error("EVL failed", ex);
+            log.error("\u001B[31m - expected errors: {}\u001B[0m", expectedErrors);
+            log.error("\u001B[31m - unexpected errors: {}\u001B[0m", ex.getUnexpectedErrors());
+            log.error("\u001B[31m - errors not found: {}\u001B[0m", ex.getErrorsNotFound());
+            log.error("\u001B[33m - expected warnings: {}\u001B[0m", expectedWarnings);
+            log.error("\u001B[33m - unexpected warnings: {}\u001B[0m", ex.getUnexpectedWarnings());
+            log.error("\u001B[33m - warnings not found: {}\u001B[0m", ex.getWarningsNotFound());
             throw ex;
         }
     }
